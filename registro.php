d1ff11f4870979c0b88849712437d3844b416644
Diego Fierro <diegoefierro@gmail.com>
2021-01-31 11:22:42 -0300

    Create registro.php

---
+++ registro.php
@@ -0,0 +1,10 @@
+<?php
+$conexion = mysql_connect("localhost", "root", "");
+mysql_select_db ("tutorial", $conexion);
+
+$usuario = $_POST('usuario');
+$contra = $_POST('contra');
+$mail = $_POST('mail');
+
+mysql_query("INSERT INTO usuarios(Usuario,Contra,Mail) VALUES('$usuario','$contra','$mail')", conexion);
+?>
